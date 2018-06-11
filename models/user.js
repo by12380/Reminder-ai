72115f6f687c1c2ca691ca88ea2c12d7e000ff33
Brian Young <by12380@gmail.com>
2018-06-10 19:43:42 -0700

    Add user model

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const userSchema = mongoose.Schema({
+    local: {
+        email: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 255,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5,
+            maxlength: 1024,
+        }
+    }
+})
+
+// generating a hash
+userSchema.methods.hashPassword = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
