3f455a2b8f9df8f554285f649b48c1ad1ef5c853
Brian Young <by12380@gmail.com>
2018-06-20 21:48:26 -0700

    Add delete reminder on dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -3,6 +3,13 @@ const APP_DATA = {
         dueDate: null,
         startDate: null
     },
+    editReminder: {
+        id: null,
+        title: null,
+        dueDate: null,
+        startDate: null,
+        memo: null
+    },
     timeInterval: null
 }
 
@@ -44,6 +51,38 @@ function handelSubmitOnReminderForm() {
     })
 }
 
+function handleReminderDelete() {
+    $('#reminder-container').on('click', '.js-close-reminder', function(){
+        const id = $(this).closest('.js-reminder').data('id');
+        bootbox.confirm({
+            message: "Are you sure you want to remove this reminder?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-danger'
+                }
+            },
+            size: 'small',
+            callback: function(result){
+                if(result){
+                    $.ajax({
+                        url: `/reminder/${id}`,
+                        method: 'delete',
+                    }).done(function(){
+                        window.location = '/dashboard';
+                    }).fail(function(err){
+                        console.log(err);
+                    })
+                }
+            }
+        });
+    })
+}
+
 function renderRemindersPartialPage() {
     $.getJSON('/reminder', function(reminders){
         renderReminders(reminders);
@@ -74,13 +113,22 @@ function renderReminders(reminders) {
             dateColorClass = 'grey-text';
         }
         $('#reminder-container').append(`
-            <div class="card m-3 ${cardBgColorClass}">
-                <!-- Card content -->
+            <div
+                class="js-reminder card m-3 ${cardBgColorClass}"
+                data-id="${reminder.id}"
+                data-title="${reminder.title}"
+                data-due-date="${reminder.dueDate}"
+                data-start-date="${reminder.startDate}"
+                data-memo="${reminder.memo}">
+
                 <div class="card-body">
-                    <!-- Text -->
+                    <button type="button" class="js-close-reminder close-reminder close waves-effect waves-light" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
                     <p class="mb-1 ${titleColorClass}">${reminder.title}</p>
                     <p class="date m-0 ${dateColorClass}">${moment(reminder.dueDate).fromNow()}</p>
                 </div>
+
             </div>
         `);
     }
@@ -90,6 +138,7 @@ function initializeDashboard() {
     initializeDateTimePicker();
     handleChangeOnDateTimePicker();
     handelSubmitOnReminderForm();
+    handleReminderDelete();
     renderRemindersPartialPage();
 }
 
